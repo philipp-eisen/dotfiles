e4db70b1f93bdf1165a0870bf70bd83d5f14461a
Philipp Eisen <8607233+philipp-eisen@users.noreply.github.com>
2026-06-15 18:18:19 +0200

    Add nix-darwin macOS management (#3)

--- flake.nix
+++ flake.nix
@@ -3,20 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager }:
     let
+      username = "phil";
+      mkPkgs = system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
       mkHome = { system, username ? "phil", modules }:
         home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true; # permit unfree-licensed packages
-          };
+          pkgs = mkPkgs system;
           modules = [
             {
               home.username = username;
@@ -29,6 +36,24 @@
         };
     in
     {
+      darwinConfigurations = {
+        phil = nix-darwin.lib.darwinSystem {
+          modules = [
+            ./darwin/system.nix
+            home-manager.darwinModules.home-manager
+            {
+              nixpkgs.pkgs = mkPkgs "aarch64-darwin";
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${username} = {
+                imports = [ ./home/common.nix ./home/darwin.nix ];
+              };
+            }
+          ];
+          specialArgs = { inherit inputs username; };
+        };
+      };
+
       homeConfigurations = {
         "phil@mac" = mkHome {
           system = "aarch64-darwin";
